baea8c50371214da80f4249c8655b1231fd1d1c6
Inggar Saputra <inggards@gmail.com>
2020-04-02 20:55:13 +0700

    login done need Client_ID for google signin

--- main.js
+++ main.js
@@ -1,22 +1,67 @@
+
+let baseUrl = 'http://localhost:3000'
+
+
 $( document ).ready(function () {
     authentication();
-    
+
 })
 
-
 function login(event) {
-    let username = $('#username').value();
-    let password = $('#password').value();
+    event.preventDefault()
 
-    console.log(username, password);
+    let email = $('#user').val();
+    let password = $('#pass').val();
+
+    $.ajax({
+
+        method : 'POST',
+        url : baseUrl + '/user/login',
+        data : {
+            email,
+            password
+        }
     
+    })
+    .done(result => {
+        localStorage.setItem('access_token', result.access_token)
+        authentication()
+    })
+    .fail(err => {
+        console.log(err);
+        
+    })
 }
 
 function authentication() {
+
     if(localStorage.access_token){
-        $('.loginTitle').hide()
+
+        $('.login-warp').hide()
+
     } else {
-        $('.loginTitle').show();
+        $('.login-warp').show();
     }
 
+}
+
+function onSignIn(googleUser) {
+
+    var id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method : 'POST',
+        url : baseUrl + '/user/googleSign',
+        data : {
+            id_token
+        }
+    })
+    .done(result => {
+        localStorage.setItem('access_token', result.access_token)
+        authentication()
+    })
+
+    .fail(err => {
+        console.log(err);
+        
+    })
 }
