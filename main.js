eccea267cafaf715a14f13f96bf53670747a15f2
Inggar Saputra <inggards@gmail.com>
2020-04-03 05:00:44 +0700

    fetch data, google done

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ $( document ).ready(function () {
         localStorage.clear();
         sessionStorage.clear();
         authentication();
+        signOut()
 
     })
         
@@ -119,6 +120,15 @@ function authentication() {
     }
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+
+    auth2.signOut().then(function () {
+
+      console.log('User signed out.');
+    });
+  }
+
 function onSignIn(googleUser) {
 
     var id_token = googleUser.getAuthResponse().id_token;
@@ -130,6 +140,8 @@ function onSignIn(googleUser) {
         }
     })
     .done(result => {
+        console.log(result);
+        
         localStorage.setItem('accessToken', result.accessToken)
         authentication()
     })
